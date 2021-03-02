de39febbc14754e859d91bd4c4c9f6dc3a3302b5
juriroemer <hoesselbarth@uni-muenster.de>
2021-03-02 22:54:30 +0100

    ProjectStepper

--- src/components/ProjectSetting.vue
+++ src/components/ProjectSetting.vue
@@ -1,5 +1,18 @@
 <template>
     <div>
-        I'm a project setting!
-    </div>
+        <v-container class="grey lighten-5">
+
+      <ProjectStepper />
+    
+  </v-container>
+</div>
 </template>
+
+<script>
+import ProjectStepper from "./ProjectStepper"
+export default {
+    components: {
+        ProjectStepper
+    }
+}
+</script>
