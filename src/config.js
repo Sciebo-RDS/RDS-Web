a5dd2873afd46e58502ed84edfa73795c9d5e54b
Peter Heiss <peter.heiss@uni-muenster.de>
2021-03-03 15:48:54 +0100

    add environment vars to config to respect dotenv

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const config = {
-    socket: {
-        server: process.env.SOCKETIO_HOST || "http://localhost:8080"
-    },
-    server: process.env.HOST || "http://localhost:8080",
     productionTip: false,
-    redirectUrl: "https://10.14.29.60/owncloud/index.php/apps/oauth2/authorize?response_type=token&client_id=Pb0UGAlNBx0Hr35mhd5DSt0jBpg1YbApwn7Hf61nREiXinZT3ucVg9K1RSKuW4Di&redirect_uri=http://localhost:8080/",
-    skipRedirect: true //disables the redirection
+    socket: {
+        server: process.env.VUE_APP_SOCKETIO_HOST
+    },
+    server: process.env.VUE_APP_FRONTENDHOST,
+    redirectUrl: process.env.VUE_APP_REDIRECTION_URL,
+    skipRedirect: process.env.VUE_APP_SKIP_REDIRECTION === "True" //disables the redirection
 }
 
 export default {
