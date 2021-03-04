dc6ed1908f0ff8f6e40ec53e5d3761ac0a767f19
juriroemer <hoesselbarth@uni-muenster.de>
2021-03-04 13:03:32 +0100

    delete project button

--- src/components/ProjectSetting.vue
+++ src/components/ProjectSetting.vue
@@ -1,10 +1,19 @@
 <template>
     <div>
         <!-- white in production-->
-        <v-container class="grey lighten-5"> 
-
+        <v-container class="grey lighten-5">
+            <v-row>
+            <v-col>
       <ProjectStepper />
-    
+            </v-col>
+            </v-row>
+            
+            <v-row align-content="end" justify="space-around">
+                <v-col cols="auto" class="mr-auto"/>
+<v-col cols="auto">
+                <v-btn tile color="error" >delete project</v-btn>
+</v-col>
+            </v-row>
   </v-container>
 </div>
 </template>
