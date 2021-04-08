7dd8d347be1c526139dcc0812f3e072bba409bfa
Peter Heiss <peter.heiss@uni-muenster.de>
2021-04-08 16:58:02 +0200

    add describos url as metadata editor

--- client/packages/codebase/src/config.js
+++ client/packages/codebase/src/config.js
@@ -5,12 +5,13 @@ const config = {
     },
     server: process.env.VUE_APP_FRONTENDHOST || "",
     redirectUrl: process.env.VUE_APP_REDIRECTION_URL,
-    skipRedirect: (process.env.VUE_APP_SKIP_REDIRECTION || "False") === "True" //disables the redirection
+    skipRedirect: (process.env.VUE_APP_SKIP_REDIRECTION || "False") === "True", //disables the redirection
+    describo: process.env.VUE_APP_DESCRIBO_URL || "http://localhost:8100"
 }
 
 export default {
     config,
-    install: function (Vue) {
+    install: function(Vue) {
         console.log(config.socket.server)
         for (const [key, value] of Object.entries(config)) {
             Vue.config[key] = value
