1dbc066c9205d1d5b1c6899788e97d7af6f41fb2
Peter Heiss <peter.heiss@uni-muenster.de>
2021-04-14 15:02:32 +0200

    remove proxy, because otherwise it circles with python proxy

--- client/vue.config.js
+++ client/vue.config.js
@@ -17,9 +17,6 @@ module.exports = {
   ],
   filenameHashing: false,
   productionSourceMap: false,
-  devServer: {
-    proxy: 'http://localhost:8080'
-  },
   configureWebpack: {
     plugins: [
       new Dotenv(dotenvArgs)
