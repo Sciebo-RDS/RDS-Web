ef3a96a0f0f30df62499ce5b9d2633ec3885d6e5
Peter Heiss <peter.heiss@uni-muenster.de>
2021-04-15 16:52:48 +0200

    remove unused env var

--- client/packages/codebase/src/config.js
+++ client/packages/codebase/src/config.js
@@ -4,8 +4,6 @@ const config = {
         server: process.env.VUE_APP_SOCKETIO_HOST || ""
     },
     server: process.env.VUE_APP_FRONTENDHOST || "",
-    redirectUrl: process.env.VUE_APP_REDIRECTION_URL,
-    skipRedirect: (process.env.VUE_APP_SKIP_REDIRECTION || "False") === "True", //disables the redirection
     describo: process.env.VUE_APP_DESCRIBO_URL || "http://localhost:8100"
 }
 
