51956a35c53cab399fdd057f599620e8f79b99a8
Peter Heiss <peter.heiss@uni-muenster.de>
2021-05-12 10:29:17 +0200

    implement supervisord for server and nginx

--- client/dev/setup/start-nginx.nginx.sh
+++ client/dev/setup/start-nginx.nginx.sh
@@ -2,6 +2,8 @@
 export EXISTING_VARS=$(printenv | awk -F= '{print $1}' | sed 's/^/\$/g' | paste -sd,);
 for file in $JSFOLDER;
 do
-  cat $file | envsubst $EXISTING_VARS | tee $file
+  mv $file $file.bak
+  cat $file.bak | envsubst $EXISTING_VARS > $file
+  rm $file.bak
 done
 nginx -g 'daemon off;'
