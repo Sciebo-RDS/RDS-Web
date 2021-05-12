d5136cb3156673e524a7d8f3ebd8b9691dfc1b37
Peter Heiss <peter.heiss@uni-muenster.de>
2021-05-12 09:02:52 +0200

    first working setup

--- client/dev/setup/start-nginx.gunicorn.sh
+++ client/dev/setup/start-nginx.gunicorn.sh
@@ -2,6 +2,9 @@
 export EXISTING_VARS=$(printenv | awk -F= '{print $1}' | sed 's/^/\$/g' | paste -sd,);
 for file in $JSFOLDER;
 do
-  cat $file | envsubst $EXISTING_VARS | tee $file
+  mv $file $file.bak
+  cat $file.bak | envsubst $EXISTING_VARS | tee $file
+  rm $file.bak
 done
-gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:5000 starter:app --log-level debug --daemon && nginx -g 'daemon off;'
+gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:5000 starter:app --log-level debug > /dev/stdout &
+nginx -g 'daemon off;' > /dev/stdout
