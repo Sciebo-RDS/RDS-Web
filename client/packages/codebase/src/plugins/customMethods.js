55594e13eb7c1ffb35d81d5abd58a880d58a1cd3
Peter Heiss <peter.heiss@uni-muenster.de>
2021-05-19 12:54:52 +0200

    format file

--- client/packages/codebase/src/plugins/customMethods.js
+++ client/packages/codebase/src/plugins/customMethods.js
@@ -1,40 +1,40 @@
 export default {
-    install: function(Vue) {
-        Vue.prototype.openPopup = async function(service, ctx) {
+    install: function (Vue) {
+        Vue.prototype.openPopup = async function (service, ctx) {
             let url = decodeURIComponent(service.authorize_url) + "&state=" + service.state;
             ctx.$_popup = window.open(url, "_blank");
             ctx.$root.$emit("showoverlay")
 
-            var checkPopup = setInterval(function() {
+            var checkPopup = setInterval(function () {
                 if (ctx.$_popup.closed) {
                     ctx.$store.dispatch("requestUserServiceList");
                     clearInterval(checkPopup);
-                    ctx.$root.$emit("hideoverlay")
+                    ctx.$root.$emit("hideoverlay");
                 }
             }, 250);
         }
 
-        Vue.prototype.parseServicename = function(value) {
+        Vue.prototype.parseServicename = function (value) {
             if (typeof value !== "string") return "";
             value = value.replace("port-", "");
             return value.charAt(0).toUpperCase() + value.slice(1);
         }
 
-        Vue.prototype.containsService = function(arr, service) {
+        Vue.prototype.containsService = function (arr, service) {
             for (const el of arr) {
                 if (el.servicename === service.servicename) return true;
             }
             return false
         }
 
-        Vue.prototype.getService = function(arr, servicename) {
+        Vue.prototype.getService = function (arr, servicename) {
             for (const el of arr) {
                 if (el.port === servicename) return el;
             }
             return undefined
         }
 
-        Vue.prototype.equalServices = function(arr, array) {
+        Vue.prototype.equalServices = function (arr, array) {
             // if the other array is a falsy value or
             // compare lengths - can save a lot of time
             if (!array || arr.length != array.length) return false;
@@ -46,7 +46,7 @@ export default {
             return true;
         }
 
-        Vue.prototype.removeDuplicates = function(arr) {
+        Vue.prototype.removeDuplicates = function (arr) {
             let servicelist = []
 
             for (const service of arr) {
@@ -58,7 +58,7 @@ export default {
             return servicelist
         }
 
-        Vue.prototype.excludeServices = function(array, arr) {
+        Vue.prototype.excludeServices = function (array, arr) {
             let product = []
             for (const service of array) {
                 if (!this.containsService(arr, service)) {
