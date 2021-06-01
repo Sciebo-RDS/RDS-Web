dda2cbe5630e8b13708406b31c2dcf2a0dc17132
Peter Heiss <peter.heiss@uni-muenster.de>
2021-06-01 17:26:19 +0200

    add frame

---
+++ client/packages/codebase/src/components/Frame.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row class="fill-height">
+      <v-col>
+        <iframe :src="source" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <slot></slot>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Frame",
+  props: {
+    source: String,
+  },
+};
+</script>
