0c991f4945092a35e35f5272aa7cf4644e8a601b
juriroemer <hoesselbarth@uni-muenster.de>
2021-06-10 17:39:41 +0200

    show warning if user has no connected services

--- client/packages/codebase/src/views/Projects.vue
+++ client/packages/codebase/src/views/Projects.vue
@@ -1,12 +1,21 @@
 <template>
   <v-main style="padding: 0px">
     <!-- move this into header bar-->
-    <v-layout column align-end wrap>
+    <v-layout column align-end wrap v-if="!!this.userservicelist">
       <v-switch
         v-model="showAll"
         inset
         :label="$gettext('show past projects')"
       ></v-switch>
+    </v-layout>
+    <v-layout column align-center wrap v-else>
+      <v-alert dense type="error" class="mb-6">
+        Please connect to at least one
+        <strong>
+          <router-link :to="{ name: 'Services' }">service</router-link>
+        </strong>
+        to use RDS
+      </v-alert>
     </v-layout>
     <ProjectList />
     <!-- put these into their own components -->
@@ -76,7 +85,7 @@
 </template>
 
 <script>
-/*import Header from '../components/Header'*/
+import { mapState } from "vuex";
 import ProjectList from "../components/Project/List.vue";
 
 export default {
@@ -92,6 +101,9 @@ export default {
         this.$store.commit("showAllProjects", val);
       },
     },
+    ...mapState({
+      userservicelist: (state) => state.RDSStore.userservicelist,
+    }),
   },
   methods: {
     collapseProjects() {
@@ -103,3 +115,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
