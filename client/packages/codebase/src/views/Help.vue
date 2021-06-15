1c913638565ec2bd4edd4690475b90d0e507d679
Peter Heiss <peter.heiss@uni-muenster.de>
2021-06-15 12:52:37 +0200

    first impression of help component

--- client/packages/codebase/src/views/Help.vue
+++ client/packages/codebase/src/views/Help.vue
@@ -1,22 +1,52 @@
 <template>
-  <div class="help">
-    <Frame :source="'/frames/' + $config.language + '/help.html'" />
-  </div>
+  <v-expansion-panels inset focusable multiple v-model="panel">
+    <v-expansion-panel
+      v-for="(item, i) in questions[this.$config.language]"
+      :key="i"
+    >
+      <v-expansion-panel-header>
+        {{ item.question }}
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        {{ item.answer }}
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script>
-// @ is an alias to /src
-import Frame from "../components/Frame.vue";
-
 export default {
   name: "Help",
-  components: {
-    Frame,
-  },
   data() {
     return {
-      gettingStarted: "https://localhost:8000",
+      panel: [],
     };
+  },
+  computed: {
+    questions() {
+      return {
+        de: [
+          {
+            question: "Dies ist die erste Frage.",
+            answer: "Dies ist die erste Antwort.",
+          },
+          {
+            question: "Dies ist die zweite Frage.",
+            answer: "Dies ist die zweite Antwort.",
+          },
+        ],
+        en: [
+          {
+            question: "This is the first question.",
+            answer: "This is the first answer.",
+          },
+          {
+            question: "This is the second question.",
+            answer: "This is the second answer.",
+          },
+        ],
+      };
+    },
   },
 };
 </script>
