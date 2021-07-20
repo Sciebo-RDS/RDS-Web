3cef3a0a1f32915a3276746a9907c861befdbda5
Peter Heiss <peter.heiss@uni-muenster.de>
2021-07-20 14:40:20 +0200

    add text for help page

---
+++ server/src/questions.py
@@ -0,0 +1,74 @@
+questions = {
+    "de": [
+        {
+            "question": "Grundlegende Informationen",
+            "answer": """Sciebo RDS ist ein Forschungsdatendienst, welcher sich in die Arbeitsabläufe der Forschenden integriert, indem neue Funktionen innerhalb der Hochschulcloud Sciebo integriert werden.
+                
+Es ist demnach nicht notwendig, neue Benutzerkonten oder weiteres anzulegen. Sie verwenden Ihr bereits vorhandenes Sciebokonto für die Authentifizierung gegenüber Sciebo RDS. Für die zu verknüpfenden Dienste, nutzen Sie auch ihre bereits bestehenden Konten, soweit vorhanden.
+
+Aktuell verknüpft Sciebo RDS verschiedene Dienste, um Ihre Daten aus der Hochschulcloud aus zu publizieren oder zu archivieren. Sie benötigen keine aufwendigen Umwandlungen oder Transfers per Hand durchführen: Das übernimmt Sciebo RDS für Sie.
+
+Für weitere Informationen, sehen Sie sich gerne auf der [offiziellen Webseite](https://www.research-data-services.org/de/) um.""",
+        },
+        {
+            "question": "Kontoverwaltung",
+            "answer": """**Services aktivieren / Deaktivieren**
+                
+Ablauf (Klickweg beschreiben)
+
+Zugangsdaten für einzelne Dienste
+
+Muss ich alle Services / Dienste aktivieren, um Sciebo RDS nutzen zu können?
+
+Was passiert mit meinen Daten, wenn ich einen Dienst deaktiviere?
+
+**Konto verwalten / löschen**
+
+Muss ich mein Sciebo RDS Konto regelmäßig verlängern (wie bei Sciebo)?
+
+Konto löschen
+
+Was passiert mit meinen Daten, wenn ich meinen Sciebo RDS Account lösche?
+
+Neues Passwort?""",
+        },
+        {
+            "question": "Daten veröffentlichen",
+            "answer": """Prinzipiell können alle Daten und -formate durch Sciebo RDS veröffentlicht werden, da keinerlei Einsicht in diese von nöten ist. Aber der Nachnutzbarkeit durch Dritte ist es natürlich förderlich, wenn Sie keine binären noch proprietäre, sondern frei verfügbare, Format bei dem Transferprozess angeben.
+Außerdem sollten die Daten eine gewisse Größe nicht überschreiten, da sonst der Transferprozess von Seiten des Dienstleisters abgebrochen werden könnte. Zum Beispiel besitzt Zenodo eine Obergrenze von 50 GB.
+
+Um Daten zu veröffentlichen, benötigen Sie ein Projekt, welches Sie durch die Konfiguration und die Angabe von Metadaten leitet. Sie können sämtliche Daten noch vor der Veröffentlichung ändern.
+Sobald Ihre Daten jedoch veröffentlich wurden, können Sie das dazugehörige Projekt nicht mehr bearbeiten.
+Falls Daten erneut veröffentlicht werden, so muss ein neues Projekt angelegt werden.
+
+Dies gewährleistet, dass Daten immer mit einer einzigartigen Identifikationsnummer (kurz DOI) versehen werden können.
+
+**Projekte anlegen**
+
+Um ein Projekt anzulegen, klicken Sie links im Menü auf Projekte. Im darauffolgenden Fenster sehen Sie unten rechts ein grünes Plus. Sobald Sie darauf klicken, wird ein neues Projekt erzeugt.
+In diesem Projekt ist nichts konfiguriert. Klicken Sie nun auf das eben erzeugte Projekt, um die Konfiguration aufzurufen. Die Konfiguration finden Sie nach dem Klick auf das entsprechende Projekt direkt darunter.
+
+Geben Sie nun alle benötigten Informationen an. Zuerst muss ein Ordner ausgewählt werden, welcher die zu veröffentlichenden Daten beinhaltet. Anschließend müssen die Dienste ausgewählt werden, welche die Daten erhalten sollen. Sollten hier keine Dienste auswählbar sein, müssen Sie erst welche unter *Dienste* hinterlegen. Wie sie das tun, steht in einem anderen Hilfekästchen.
+Sobald nun alle Informationen hinterlegt wurden, können Sie auf *Weiter* drücken. Nun sehen Sie ein weiteres Fenster, worin Sie ihre Metadaten eintragen können. Wie dies zu verwenden ist, steht ebenfalls in einem weiteren Hilfekästchen. Im letzten Fenster können Sie nun nocheinmal Ihre Angaben einsehen und ggfs. zurückgehen, um die Informationen anzupassen.
+Sollten Sie mit Ihren Angaben zufrieden sein, so können Sie nun die Daten veröffentlichen. Herzlichen Glückwunsch: Ihre Daten sind nun auf den angegeben Diensten veröffentlicht worden und stehen der Allgemeinheit zur Verfügung.
+
+Sollten Sie falsche Angaben gemacht haben bei den Metadaten oder der Konfiguration, so müssen sie diese per Hand, falls möglich, im entsprechenden Dienst anpassen. Sciebo RDS bietet dafür noch keine Funktionalität.
+"""
+        }, {
+            "question": "Metadaten verwalten",
+            "answer": """
+Ich habe falsche oder unvollständige (Meta)daten veröffentlicht, wie kann ich Daten ändern?
+            """
+        }
+    ],
+    "en": [
+        {
+            "question": "This is the first question.",
+            "answer": "This is the first answer.",
+        },
+        {
+            "question": "This is the second question.",
+            "answer": "This is the second answer.",
+        },
+    ],
+}
