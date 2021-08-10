7e6772df8edfd278948ea7b174647083b9ad822a
juriroemer <hoesselbarth@uni-muenster.de>
2021-08-10 16:41:33 +0200

    adds new iFrames start.html and welcome.html

--- client/packages/codebase/src/components/Service/List.vue
+++ client/packages/codebase/src/components/Service/List.vue
@@ -55,7 +55,6 @@ export default {
           return true;
         }
       }
-
       return false;
     },
   },
--- client/packages/codebase/src/views/Home.vue
+++ client/packages/codebase/src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    <Frame :source="'/frames/' + $config.language + '/start.html'"></Frame>
+    <!--<v-layout column align-end wrap v-if="userHasServicesConnected">
+      at least one service is connected
+    </v-layout>-->
+    <v-layout>
+      <Frame :source="'/frames/' + $config.language + '/welcome.html'"></Frame>
+    </v-layout>
   </div>
 </template>
 
@@ -17,6 +22,28 @@ export default {
     return {
       gettingStarted: "https://localhost:8000",
     };
+  },
+  computed: {
+    userHasServicesConnected() {
+      //hardcoded filter for owncloud, change
+      if (this.userservicelist.length > 1) {
+        return true;
+      }
+      return false;
+    },
+  },
+  mounted() {
+    window.addEventListener("message", this.receiveMessage);
+  },
+  beforeDestroy() {
+    window.removeEventListener("message", this.receiveMessage);
+  },
+  methods: {
+    receiveMessage(event) {
+      if (event.data.func === "redirect") {
+        this.$router.push(event.data.target);
+      }
+    },
   },
 };
 </script>
