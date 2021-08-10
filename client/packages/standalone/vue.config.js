faeaa2928844013db6b3d377e764491bb8b77bbe
Peter Heiss <peter.heiss@uni-muenster.de>
2021-08-10 08:36:04 +0200

    unneded condition

--- client/packages/standalone/vue.config.js
+++ client/packages/standalone/vue.config.js
@@ -11,7 +11,7 @@ const dotenvArgs = {
 };
 
 let publicPath = '/';
-if (process.env.NODE_ENV === 'production' && !!process.env.VUE_APP_BASE_URL) {
+if (process.env.NODE_ENV === 'production') {
     publicPath = ".";
 }
 
