23017a0e69cd264f7447c4aa029bd0739adce4a7
Peter Heiss <peter.heiss@uni-muenster.de>
2021-09-08 11:09:56 +0200

    fix user redirect after activation

--- client/packages/codebase/src/views/Wizard.vue
+++ client/packages/codebase/src/views/Wizard.vue
@@ -144,7 +144,7 @@ export default {
       ) {
         console.log("found ownCloud in storage");
         clearInterval(timer);
-        this.finishWizard("/services");
+        this.finishWizard();
       }
     }, 1000);
   },
