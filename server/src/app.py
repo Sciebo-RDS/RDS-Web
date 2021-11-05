6dfa38ea0410653427a629bfc97742dbdd27e7d1
Peter Heiss <peter.heiss@uni-muenster.de>
2021-11-05 18:19:52 +0100

    fix metrics

--- server/src/app.py
+++ server/src/app.py
@@ -5,6 +5,7 @@ import redis_pubsub_dict
 from rediscluster import RedisCluster
 from flask import Flask
 import uuid
+import logging
 import os
 import json
 from flask_socketio import SocketIO
@@ -92,9 +93,10 @@ app = Flask(__name__,
             static_folder=os.getenv(
                 "FLASK_STATIC_FOLDER", "/usr/share/nginx/html")
             )
-
+app.logger.setLevel(logging.DEBUG)
 
 metrics = PrometheusMetrics(app)
+
 install_all_patches()
 
 tracer_config = {
