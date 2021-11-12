f266ea7979129bb9cfb77785ee8e75b51d99c878
Peter Heiss <peter.heiss@uni-muenster.de>
2021-11-12 13:51:07 +0100

    Revert "found out, that there are logging params"
    This reverts commit e323f997470da5aa6ec07a4a437ea9d13baa5843.

--- server/src/app.py
+++ server/src/app.py
@@ -91,8 +91,7 @@ else:
 
 app = Flask(__name__,
             static_folder=os.getenv(
-                "FLASK_STATIC_FOLDER", "/usr/share/nginx/html"),
-            logger=True
+                "FLASK_STATIC_FOLDER", "/usr/share/nginx/html")
             )
 
 app.logger.setLevel(logging.DEBUG)
--- server/starter.py
+++ server/starter.py
@@ -6,11 +6,8 @@ except ImportError:
 
 from src.server import app, socketio
 import os
-import logging
 from pathlib import Path
 from dotenv import load_dotenv
-
-logging.basicConfig(level=logging.DEBUG)
 
 env_path = Path('..') / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -19,6 +16,5 @@ if __name__ == "__main__":
     socketio.run(app,
                  debug=os.getenv("DEV_FLASK_DEBUG", "False") == "True",
                  host="0.0.0.0",
-                 port=8080,
-                 log_output=True
+                 port=8080
                  )
